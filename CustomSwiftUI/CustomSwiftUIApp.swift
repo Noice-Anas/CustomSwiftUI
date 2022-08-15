fb7dcd22959c95805bd917adef1718f4e9fe3b35
Noice_Anas <anas.alhalabi2002@gmail.com>
2022-08-15 11:35:52 +0300

    Initial Commit

---
+++ CustomSwiftUI/CustomSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftUIApp.swift
+//  CustomSwiftUI
+//
+//  Created by Noice_anas on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
